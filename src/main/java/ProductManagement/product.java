a439dc835923098c111a965ac5066c40e5b89ec4
ANANDU <ANANDU@192.168.1.58>
2026-03-04 18:27:23 +0530

    test1

--- src/main/java/ProductManagement/product.java
+++ src/main/java/ProductManagement/product.java
@@ -8,7 +8,11 @@ public class product {
 	private WebDriver driver;
 	
 	private By ProductManagement = By.xpath("//span[contains(text(),'Product Management')]");
-	//
+	private By productsidemenu = By.xpath("//button[text()='Product']");
+	private By AddProduct = By.xpath("//button[text()='Add Product']");
+	private By ProductName = By.id("productname");
+	private By Category = By.xpath("(//span[@class='text-[#71717A]'])[1]");
+			
 	
 	public void productmanagementdrop() {
 		driver.findElement(ProductManagement).click();
